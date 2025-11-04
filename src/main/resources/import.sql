a976fcef4c1e3d082d0b43f5bbe4e93464e78bca
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-11-03 23:49:33 -0300

    Seeding the database for testing

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_users (nome, email, idade, profissao, salario_Bruto, salario_Liquido) VALUES ('Henrique Alves', 'henrique@gmail.com', 22, 'Escrevente de cartório', 2560.0, 2200.0);
+INSERT INTO tb_users (nome, email, idade, profissao, salario_Bruto, salario_Liquido) VALUES ('Bianca', 'bianca@gmail.com', 22, 'Desempregada', 0000.0, 0000.0);
+
+
