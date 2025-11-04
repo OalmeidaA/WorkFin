ecca325c09efb14251eb3c936906751e90e3ebd6
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-11-03 21:15:51 -0300

    First commit

---
+++ src/main/java/com/GMHDigital/API_WorkFin/ApiWorkFinApplication.java
@@ -0,0 +1,13 @@
+package com.GMHDigital.API_WorkFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorkFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWorkFinApplication.class, args);
+	}
+
+}
