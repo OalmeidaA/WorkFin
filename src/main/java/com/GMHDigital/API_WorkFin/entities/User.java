336d35e735f1d211fb1f79a99884075da2d0b5e8
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-11-03 21:19:13 -0300

    created entity user

---
+++ src/main/java/com/GMHDigital/API_WorkFin/entities/User.java
@@ -0,0 +1,129 @@
+package com.GMHDigital.API_WorkFin.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private Integer idade;
+	private String profissao;
+	private LocalDate dataInicio;
+	private LocalDate dataFinal;
+	private Boolean feriasVencidas;
+	
+	public User() {
+	}
+
+	public User(Long id, String nome, String email, Integer idade, String profissao, LocalDate dataInicio,
+			LocalDate dataFinal, Boolean feriasVencidas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.profissao = profissao;
+		this.dataInicio = dataInicio;
+		this.dataFinal = dataFinal;
+		this.feriasVencidas = feriasVencidas;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(LocalDate dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+	public Boolean getFeriasVencidas() {
+		return feriasVencidas;
+	}
+
+	public void setFeriasVencidas(Boolean feriasVencidas) {
+		this.feriasVencidas = feriasVencidas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
